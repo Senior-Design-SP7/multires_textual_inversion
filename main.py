b5f10e3ca444a02e28cc3b546628ff24d5200341
laithaustin <laithaustin@utexas.edu>
2023-01-18 14:29:55 -0600

    basic source code

---
+++ main.py
@@ -0,0 +1,22 @@
+# backend/main.py
+
+import uuid
+
+import cv2
+import uvicorn
+from fastapi import File
+from fastapi import FastAPI
+from fastapi import UploadFile
+import numpy as np
+from PIL import Image
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Welcome from the API"}
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8080)
