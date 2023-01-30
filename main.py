e91f8121a495abd8ce16e150129d537cb1b34a00
laithaustin <laithaustin@utexas.edu>
2023-01-30 15:50:56 -0600

    implemented promptModel code - needs testing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import uuid
 
-import cv2
+# import cv2
 import uvicorn
 from fastapi import FastAPI, UploadFile
 import numpy as np
@@ -10,24 +10,27 @@ from PIL import Image
 from typing import List
 import os
 
+from pipeline import DreamBoothMultiResPipeline
+import torch
+
 MODEL_NAME="runwayml/stable-diffusion-v1-5"
 
 #short helper to format cmd for training new concepts
 def trainConcept(conceptDir, conceptName):
-    trainCMD = "accelerate launch train_dreambooth.py \
-                --pretrained_model_name_or_path={} \
-                --instance_data_dir={} \
-                --output_dir=dreambooth_outputs/multires_100/{} \
-                --instance_prompt='S' \
-                --resolution=512 \
-                --train_batch_size=1 \
-                --gradient_accumulation_steps=4 --gradient_checkpointing \
-                --use_8bit_adam \
-                --learning_rate=5e-6 \
-                --lr_scheduler='constant' \
-                --lr_warmup_steps=0 \
-                --max_train_steps=100 ".format(MODEL_NAME, conceptDir, conceptName)
-    return trainCMD
+	trainCMD = "accelerate launch train_dreambooth.py \
+				--pretrained_model_name_or_path={} \
+				--instance_data_dir={} \
+				--output_dir=dreambooth_outputs/multires_100/{} \
+				--instance_prompt='S' \
+				--resolution=512 \
+				--train_batch_size=1 \
+				--gradient_accumulation_steps=4 --gradient_checkpointing \
+				--use_8bit_adam \
+				--learning_rate=5e-6 \
+				--lr_scheduler='constant' \
+				--lr_warmup_steps=0 \
+				--max_train_steps=100 ".format(MODEL_NAME, conceptDir, conceptName)
+	return trainCMD
 
 app = FastAPI()
 
@@ -36,21 +39,33 @@ app = FastAPI()
 # TODO Make it so that we save the embeddings + a unique userid to our database
 @app.post("/addConcept/")
 def create_upload_dir(name: str, files: List[UploadFile]):
-    # save images to directory for reference
-    os.system('mkdir ' + name) #replace this with actual storage system for images
-    dir = os.getcwd() + "/" + name
-    for f in files:
-        file_location = dir + "/" + f.filename
-        with open(file_location, "wb+") as file_object:
-            file_object.write(f.file.read())
+	# save images to directory for reference
+	os.system('mkdir ' + name) #replace this with actual storage system for images
+	dir = os.getcwd() + "/" + name
+	for f in files:
+		file_location = dir + "/" + f.filename
+		with open(file_location, "wb+") as file_object:
+			file_object.write(f.file.read())
 
-    # call training cmd for giannis's model
-    # os.system(trainConcept(dir, name))
-    return "SUCCESS!"
+	# call training cmd for giannis's model
+	# os.system(trainConcept(dir, name))
+	return "SUCCESS!"
+
+# POST request that receives an image based on a prompt that's given
+# TODO Make it so that we have a dictionary of trained concepts for each user on mongoDB
+# TODO Make the prompt customizable
+@app.post("/promptModel/")
+def create_upload_dir(name: str, location: str):
+	pipe = DreamBoothMultiResPipeline.from_pretrained(f"{location}", use_auth_token=True)
+	pipe = pipe.to("cuda")
+	image = pipe(f"An image of <{name}(0)>")[0]
+	loc = f"out_image.png"
+	image.save(loc)
+	return "SUCCESS!"
 
 @app.get("/")
 def read_root():
-    return {"message": "Welcome from the API"}
+	return {"message": "Welcome from the API"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080)
+	uvicorn.run("main:app", host="0.0.0.0", port=8080)
